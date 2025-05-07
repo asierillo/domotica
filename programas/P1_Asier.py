cc768f7b78522867d6f6bc1529a37a21486ae099
asierillo <AsierPino@iesteis.net>
2025-05-07 14:26:09 +0200

    Update P1_Asier.py

--- programas/P1_Asier.py
+++ programas/P1_Asier.py
@@ -2,9 +2,13 @@
 from microbit import*
 import music
 
-pir = pin15
+pir = pin15.read_digital()
+np = neopixel.NeoPixel(pin13)
+led = pin14
 
 while True:
+  if pir ==1:
+    music.play(music.RINGTONE)
   if pir.read_digital():
     music.play(music.RINGTONE)
     sleep(100)
@@ -16,6 +20,11 @@ while True:
     np.show()
     led.write_digital(1)
     sleep(100)
+     led.write_digital(0)
+  sleep(100)
+  led.write_digital(1)
+  sleep(500)
+  led.write_digital(0)
   else:
   for lo que quieras poner in range(5):
   np[0] = (0, 0, 0)
@@ -23,3 +32,9 @@ while True:
   np.show()
   led.write_digital(0)
   sleep(100)
+  led.write_digital(1)
+  sleep(500)
+  led.write_digital(0)
+  np.clear
+display.show(Image.ANGRY)
+display.clear
